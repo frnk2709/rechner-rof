22bfd85e6ca05c8d226549cf5c3c501a96dcd2bd
frnk2709 <fstadelmann80@gmail.com>
2025-08-13 21:44:25 +0200

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -2,6 +2,7 @@ import streamlit as st
 from afa_wg_streamlit import show_afa_wg
 from afa_gebaeude import show_afa_gb
 from ertragswertverfahren.ertragswertferfahren_rechner_streamlit import show_ertragswertverfahren_rechner
+from entfernungspauschale_st import show_entfernungspauschale
 
 # Session-State initialisieren
 if 'page' not in st.session_state:
@@ -20,6 +21,8 @@ if st.session_state.page == 'menu':
         switch_page('afa_gb')
     if st.button('Ertragswertferfahren'):
         switch_page('ertragswertverfahren')
+    if st.button('Entfernungspauschale'):
+    switch_page('entfernungspauschale')
 
 elif st.session_state.page == 'afa_wg':
     show_afa_wg(switch_page)
@@ -27,3 +30,5 @@ elif st.session_state.page == 'afa_gb':
     show_afa_gb(switch_page)
 elif st.session_state.page == 'ertragswertverfahren':
     show_ertragswertverfahren_rechner(switch_page)
+elif st.session_state.page == 'entfernungspauschale':
+    show_entfernungspauschale(switch_page)
